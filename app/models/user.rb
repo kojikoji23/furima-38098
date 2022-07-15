0a61384585e66bf13ae9074d1c4e090cef751552
kojikoji23 <shokojima23@gmail.com>
2022-07-15 19:38:51 +0900

    エラーメッセージの表示の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters"}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters"}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: "Input full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: "Input full-width katakana characters"}
   validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
 
   has_many :items
   has_many :orders
