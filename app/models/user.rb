4baea06ca791e296d9f65ee9f57cdf6ae4ae444e
kojikoji23 <shokojima23@gmail.com>
2022-07-17 16:03:52 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'Input full-width katakana characters' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'Input full-width katakana characters' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/u, message: 'Input full-width katakana characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/u, message: 'Input full-width katakana characters' }
   validates :birthday, presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
