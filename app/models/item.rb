7ecbe0ccc0b1011d614412864d6761939645f16b
kojikoji23 <shokojima23@gmail.com>
2022-07-18 15:58:41 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :ship_day
+
+  validates :title, :content, :price, presence: true
+
+  validates :category_id, :condition_id, :charge_id, :prefecture_id, :ship_day_id, numericality: { other_than: 1 , message: "can't be blank" } 
 end
