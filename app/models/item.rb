735df0fb9b26b3cc5ee39cf656b2ce413d3f5c84
kojikoji23 <shokojima23@gmail.com>
2022-07-18 19:49:35 +0900

    データの保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,13 @@ class Item < ApplicationRecord
   belongs_to :charge
   belongs_to :prefecture
   belongs_to :ship_day
+  has_one_attached :image
+  belongs_to :user
 
-  validates :title, :content, :price, presence: true
+  validates :title, :content, :image, presence: true
 
   validates :category_id, :condition_id, :charge_id, :prefecture_id, :ship_day_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is invalid" }
+
 end
