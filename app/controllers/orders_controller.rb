6198a4fae1e00213a53611b7eb3658fd9f86eb50
kojikoji23 <shokojima23@gmail.com>
2022-07-26 11:57:28 +0900

    データ保存、エラー表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,22 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order_destination = OrderDestination.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(:zip, :prefecture_id, :city, :address, :building, :tel, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,8 +3,4 @@ class Destination < ApplicationRecord
   belongs_to :order
   belongs_to :prefecture
 
-  validates :city, :address, presence: true
-  validates :zip, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :tel, presence: true, format: { with: ^0\d{10,11}$, message: 'is invalid' }
 end
